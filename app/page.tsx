89224f3dae5e2d89d67c1d07d1d6ddfd19d434aa
pranavcpawar <pranavcpawar@outlook.com>
2024-03-30 20:54:35 +0530

    read data

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,18 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { useAccount } from "wagmi";
 import { abi } from "../abi/abi";
 import { ethers, Contract } from "ethers";
+import { useEffect } from "react";
 
 export default function Home() {
   const account = useAccount();
-  const provider = new ethers.JsonRpcProvider();
-
-  const contract = new Contract("0x764594F8e9757edE877B75716f8077162B251460", abi);
-
+  const provider = new ethers.BrowserProvider(window.ethereum);
+  const contract = new Contract("0x764594F8e9757edE877B75716f8077162B251460", abi, provider);
+    async function readData() {
+      const collateral = await contract.getUserAccountData(account.address!);
+      console.log(collateral);
+    };
+    readData();
+  
   return (
     <main className="flex flex-col items-center justify-center gap-[20px] p-4">
       <ConnectButton showBalance />
